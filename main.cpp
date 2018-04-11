3f12f80bec085931d8e1b05bd1f117a0673be788
Du Park <dupark3@gmail.com>
2018-04-11 15:28:59 -0400

    Works for negatives but does not treat overflow correctly

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //If it is overflow, return MAX_INT.
 
 #include <climits> //INT_MAX
+#include <cmath> // abs
 #include <iostream>
 
 using namespace std;
@@ -10,24 +11,32 @@ using namespace std;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (divisor != 0){
-            int answer = 0;
-            while (dividend > divisor){
-                dividend -= divisor;
-                ++answer;
-            }
-            return answer;
-        } else {
+        if (divisor == 0)
             return INT_MAX;
+
+        bool opposite_signs = false;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)){
+            opposite_signs = true;
+            dividend = dividend - dividend - dividend; // flip sign
         }
 
+        int answer = 0;
+        while (abs(dividend) >= abs(divisor)){
+            dividend -= divisor;
+            ++answer;
+        }
+
+        if (opposite_signs)
+            return answer - answer - answer;
+        else
+            return answer;
     }
 };
 
 int main()
 {
     Solution s;
-    int answer = s.divide(5, 2);
+    int answer = s.divide(5, -2);
     cout << answer << endl;
 
     return 0;
